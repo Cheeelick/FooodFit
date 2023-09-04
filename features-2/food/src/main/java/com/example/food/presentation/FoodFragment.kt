ae9c99502b0a7e337ce306dd7b17c13e8811e0c7
Cheeelick <Artemida5645@mail.ru>
2023-09-04 10:32:44 +0300

    Навигация работает !!!

--- features-2/food/src/main/java/com/example/food/presentation/FoodFragment.kt
+++ features-2/food/src/main/java/com/example/food/presentation/FoodFragment.kt
@@ -1,7 +1,13 @@
 package com.example.food.presentation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.food.R
 
 class FoodFragment: Fragment(R.layout.fragment_food) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- features-2/main/src/main/java/com/example/main/MainFragment.kt
+++ features-2/main/src/main/java/com/example/main/MainFragment.kt
@@ -15,9 +15,9 @@ class MainFragment: Fragment(R.layout.fragment_main){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
-//        val navController = (childFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment).navController
-//        bottomNavigationView.setupWithNavController(navController)
+        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = (childFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment).navController
+        bottomNavigationView.setupWithNavController(navController)
 
 
 
